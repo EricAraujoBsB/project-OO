63fff216a59640399997f7116a654268cc496929
Eric Araujo desktop <egericaraujo@gmail.com>
2022-08-17 11:15:53 -0300

    gets e sets

--- Project-OO/src/model/Medicacao.java
+++ Project-OO/src/model/Medicacao.java
@@ -5,6 +5,32 @@ public class Medicacao {
 	private Doenca doenca;
 	private String hora_medicacao;
 	
+	//construtor
+	public Medicacao(String n, Doenca d, String h) {
+		nome = n;
+		doenca = d;
+		hora_medicacao = h;
+	}
+	
+	//GETs e SETs
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Doenca getDoenca() {
+		return doenca;
+	}
+	public void setDoenca(Doenca d) {
+		this.doenca = d;
+	}
+	public String getHoraMedicacao() {
+		return hora_medicacao;
+	}
+	public void setHoraMedicacao(String h) {
+		this.hora_medicacao = h;
+	}
 	public void definir_hora_medicacao() {
 		
 	}
