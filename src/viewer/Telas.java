414587e8b74c20d3fa9633e1b03243940fa89cc9
Pedro Sorroche <pedrosorroche.dev@gmail.com>
2022-09-01 23:26:41 -0300

    adiciona JList

--- src/viewer/Telas.java
+++ src/viewer/Telas.java
@@ -23,6 +23,7 @@ public class Telas {
 		tela.add(reg);
 				
 		tela.setSize(450,700);
+		
 		tela.setLayout(null);
 		tela.setVisible(true);
 		
@@ -53,7 +54,21 @@ public class Telas {
 
 		JLabel inten = new JLabel("Intensidade da Dor");
 		inten.setBounds(120, 210, 120, 100);
+		String[] n_inten = new String[10];
+		n_inten[0] = "1";
+		n_inten[1] = "2";
+		n_inten[2] = "3";
+		n_inten[3] = "4";
+		n_inten[4] = "5";
+		n_inten[5] = "6";
+		n_inten[6] = "7";
+		n_inten[7] = "8";
+		n_inten[8] = "9";
+		n_inten[9] = "10";
+		JList intens = new JList<>(n_inten);
+		intens.setBounds(115, 270, 120, 30);
 		tela2.add(inten);
+		tela2.add(intens);
 
 		JLabel local = new JLabel("Local da Dor");
 		local.setBounds(130, 310, 120, 100);
