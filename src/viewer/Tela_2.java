6e23a14c1db4ba2643f6ced6fe8378f9432f5d96
Pedro Sorroche <pedrosorroche.dev@gmail.com>
2022-09-05 23:23:38 -0300

    modela janela 2

--- src/viewer/Tela_2.java
+++ src/viewer/Tela_2.java
@@ -8,6 +8,8 @@ public class Tela_2 extends JFrame {
 		tela2.setSize(450,700);
 		tela2.setLayout(null);
 		
+	
+		
 		JLabel data = new JLabel("Data");
 		JTextField data_in = new JTextField(" ");
 		data_in.setBounds(50, 70, 100, 25);
@@ -24,42 +26,48 @@ public class Tela_2 extends JFrame {
 	
 		JLabel tempo = new JLabel("Tempo de Duração");
 		JTextField duracao_in = new JTextField(" ");
-		duracao_in.setBounds(130, 170, 100, 25);
-		tempo.setBounds(125, 110, 120, 100);
+		duracao_in.setBounds(50, 175, 100, 25);
+		tempo.setBounds(50, 110, 120, 100);
 		tela2.add(tempo);
 		tela2.add(duracao_in);
 	
 		JLabel inten = new JLabel("Intensidade da Dor");
-		inten.setBounds(120, 210, 120, 100);
-		String[] n_inten = {"1","2","3","4","5","6","7","8","9","10"};
-		JList list_intens = new JList<>(n_inten);
-		list_intens.setBounds(115, 270, 120, 30);
+		JLabel num = new JLabel("(1 a 10)");
+		inten.setBounds(250, 110, 120, 100);
+		num.setBounds(280,123,120,100);
+		JTextField intens_in = new JTextField(" ");
+		intens_in.setBounds(285, 185, 30, 30);
 		tela2.add(inten);
-		tela2.add(list_intens);
+		tela2.add(num);
+		tela2.add(intens_in);
 	
 		JLabel local = new JLabel("Local da Dor");
-		local.setBounds(130, 310, 120, 100);
-		String[] locals = {};
+		local.setBounds(60, 210, 120, 100);
+		String[] locals = {"Ambas laterais", "Atrás dos olhos", "Lado Direito", "Lado Esquerdo", "Nuca", "Testa"};
 		JList list_locals = new JList<>(locals);
-		list_locals.setBounds(115, 370, 120, 30);
+		list_locals.setBounds(50, 270, 120, 110);
 		tela2.add(local);
 		tela2.add(list_locals);
 	
 		JLabel causa = new JLabel("Provável Causa");
-		causa.setBounds(120, 410, 120, 100);
-		String[] causes ={};
+		causa.setBounds(255, 210, 120, 100);
+		String[] causes ={"Estresse", "Odores Fortes", "TPM", "Sono Irregular"};
 		JList list_causes = new JList<>(causes);
-		list_causes.setBounds(115, 470, 120, 30);
+		list_causes.setBounds(245, 270, 120, 100);
 		tela2.add(causa);
 		tela2.add(list_causes);
 	
 		JLabel med = new JLabel("Medicação");
-		med.setBounds(130, 510, 120, 100);
-		String[] medicines ={};
+		med.setBounds(170, 360, 120, 100);
+		String[] medicines ={"Paracetamol", "Dipiroma"};
 		JList list_medicines = new JList<>(medicines);
-		list_medicines.setBounds(115, 570, 120, 30);
+		list_medicines.setBounds(145, 420, 120, 35);
 		tela2.add(med);
 		tela2.add(list_medicines);
+		
+		JButton sub=new JButton("Submit");
+		sub.setBounds(150, 550, 100, 50);
+		tela2.add(sub);
 	
 		tela2.setVisible(true);
 	}
