f04239285e4ab0bb58b7d78e1306d0ec9f6667b7
Eric Araujo desktop <egericaraujo@gmail.com>
2022-09-11 01:37:50 -0300

    controller

--- src/controller/Tela_1_Controller.java
+++ src/controller/Tela_1_Controller.java
@@ -4,8 +4,9 @@ import model.*;
 public class Tela_1_Controller {
 	public boolean registrarEntrada(String data, String hora, int duracao, int intensidade, String local, String causa, String medicacao) {
 		if(data != null && data.length() > 0 && hora != null && hora.length() >0 && duracao > 0 && intensidade > 0 && local != null && local.length()  > 0 && causa != null && causa.length() > 0 && medicacao != null && medicacao.length() > 0)) {
-			/* Dor_de_Cabeca dor_de_Cabeca = new Dor_de_Cabeca */
-			Dor_de_Cabeca.definir_dor(intensidade, local, causa, medicacao);
+			Dor_de_Cabeca dor_de_Cabeca = new Dor_de_Cabeca("dor de cabeça", "nausea", "descanso",medicacao, intensidade, local, causa);
+			
+			//Dor_de_Cabeca.definir_dor(intensidade, local, causa, medicacao);
 			
 		}
 	}
